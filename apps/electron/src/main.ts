2ee5371950b6a1c2c271561cb46418ccbd9a6be7
iamDyeus <dyeusyt@gmail.com>
2026-04-19 15:14:23 +0530

    feat: add qwendean icon to main window and update image assets

--- apps/electron/src/main.ts
+++ apps/electron/src/main.ts
@@ -15,6 +15,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, "../../images/qwendean.png"),
     webPreferences: {
       devTools: inDevelopment,
       contextIsolation: true,
